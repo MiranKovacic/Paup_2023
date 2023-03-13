69baeed7ad5e84b249765f616719b5fb01d91abf
Miran Kovačić <miran.kovacic@supersport.hr>
2023-03-13 13:15:41 +0100

    Add project files.

---
+++ Controllers/PrviController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paup_2023.Controllers
+{
+    public class PrviController : Controller
+    {
+        // GET: Prvi
+        public ActionResult Index()
+        {
+            ViewBag.Lokacija = "Međimursko Veleučilište u Čakovcu";
+            return View();
+        }
+
+        public ActionResult Druga()
+        {
+            ViewBag.Ustanova = "Međimursko veleučilište";
+            ViewData["Lokacija"] = "Čakovec";
+            return View();
+        }
+
+        public ActionResult Treca(string poruka, int broj=1)
+        {
+            ViewBag.Poruka = poruka;
+            ViewBag.Broj = broj;
+            return View();
+        }
+
+        public ActionResult Student()
+        {
+            ViewBag.Ime = "Ime";
+            ViewBag.Prezime = "Prezime";
+            ViewBag.GodinaRodjenja = "1999";
+            return View();
+        }
+    }
+}
